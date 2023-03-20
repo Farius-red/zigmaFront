de5831985135eed21c17a115bc4fc1f1e5761be8
EstebanJuliao <zigmainflables@gmail.com>
2023-03-20 00:45:42 -0500

    Mejora en inicio Landig page Zigma

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,8 +1,9 @@
-import { Observable } from 'rxjs';
-import { MenuSvcService } from './../../core/servicios/menuSvc/menu-svc.service';
-import { Component, OnInit } from '@angular/core';
-import { MenuModel } from 'src/app/core/modelos/menu/menu.Model';
 
+import { Observable, delay } from 'rxjs';
+import { MenuSvcService } from './../../core/servicios/menuSvc/menu-svc.service';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MenuModel } from 'src/app/core/modelos/menu/menu.Model';
+import { Navigation } from "swiper";
 
 @Component({
   selector: 'app-inicio',
@@ -12,12 +13,51 @@ import { MenuModel } from 'src/app/core/modelos/menu/menu.Model';
 export class InicioPage implements OnInit {
   menuId:string = "inicio"
   menu:Observable<MenuModel[]>
-  constructor(private  menuSvc: MenuSvcService) { }
+
+  imagenes = [{
+    src: '../../../assets/imagenes/banerProductosRecreativos.jpg',
+    alt: 'zigma inflables banner 1'
+  },
+  {
+    src: '../../../assets/imagenes/banner2.png',
+    alt: 'zigma inflables banner 2'
+  },
+  {
+    src: '../../../assets/imagenes/banner4.jpg',
+    alt: 'zigma inflables banner 3 '
+  },
+
+  {
+    src: '../../../assets/imagenes/banerZigma5.jpg',
+    alt: 'zigma inflables banner 4 '
+  },
+ {
+  src: '../../../assets/imagenes/bannerZigma6.jpg',
+    alt: 'zigma inflables banner 5 '
+ }
+  
+  
+]
+
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    loop: true ,
+    autoplay:{
+      delay: 4000
+    }
+  };
+
+  constructor(private  menuSvc: MenuSvcService ,
+
+    ) { }
 
   ngOnInit() {
-    
+     
   }
 
+  moverSlideAdelante(){
+  }
+	
   
-
 }
